0c3df8fa53d677ab294bcd0b9655ed6152e2d309
Charles Seymour <charlesseymour@yahoo.com>
2019-08-11 16:24:03 -0500

    Revised tags as single field in entries table

--- index.php
+++ index.php
@@ -2,11 +2,34 @@
 
 require_once("database.php"); 
 
-try {
-	$results = $db->query('select * from entries order by date desc');
-} catch(Exception $e) {
-	echo $e->getMessage();
-	die();
+if (isset($_GET["tag"])) {
+	try {
+		$tag = filter_input(INPUT_GET, 'tag', FILTER_SANITIZE_STRING);
+		$tag1 = $tag . " %";
+		$tag2 = "% " . $tag;
+		$tag3 = "% " . $tag . " %";
+		$results = $db->prepare('SELECT * FROM entries WHERE 
+								tags LIKE ? OR
+								tags LIKE ? OR
+								tags LIKE ? OR 
+								tags = ?
+								ORDER BY date DESC');
+		$results->bindParam(1, $tag1);
+		$results->bindParam(2, $tag2);
+		$results->bindParam(3, $tag3);
+		$results->bindParam(4, $tag);
+		$results->execute();
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		die();
+	}
+} else {
+	try {
+		$results = $db->query('select * from entries order by date desc');
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		die();
+	}
 }
 
 $entries = $results->fetchAll(PDO::FETCH_ASSOC);
@@ -30,30 +53,23 @@ $entries = $results->fetchAll(PDO::FETCH_ASSOC);
             <div class="container">
                 <div class="entry-list">
 					<?php
+					if (isset($_GET["tag"])) {
+						echo('<h3 style="text-align: center; color: #678f89;">Entries tagged: "' . $_GET["tag"] . '"</h3><br>');
+					}
 					foreach($entries as $entry) {
 						$fullDate = date('F j, Y', strtotime($entry['date']));
-						try {
-							$results = $db->prepare('select tag from entries
-												   join entries_tags on entries.id = entries_tags.entry_id
-												   join tags on entries_tags.tag_id = tags.id
-												   where entries.id = ?');
-							$results->bindParam(1, $entry['id']);
-							$results->execute();
-							} catch(Exception $e) {
-								echo $e->getMessage();
-								die();
-						}
-						$tags = $results->fetchAll(PDO::FETCH_ASSOC);
 						echo <<<EOT
 						<article>
 							<h2><a href="detail.php?id={$entry['id']}">{$entry['title']}</a></h2>
 							<time datetime="{$entry['date']}">{$fullDate}</time>
 EOT;
-						if (!empty($tags)) {
+						if (!empty($entry["tags"])) {
+							$tags = explode(" ", $entry["tags"]);
 							echo '<div class="tag-container"><span class="tag-label">tags:</span>';
 							foreach($tags as $tag) {
-								echo '<span class="tag"><a href="index.php?search=' . $tag["tag"] .
-								'">' . $tag["tag"] . '</a></span>';
+								$tag = trim($tag);
+								echo '<span class="tag"><a href="index.php?tag=' . $tag .
+								'">' . $tag . '</a></span>';
 							}
 							echo('</div>');
 						}
