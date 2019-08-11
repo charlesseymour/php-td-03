62358ed1824eef3f568adc59315943e3e885d570
Charles Seymour <charlesseymour@yahoo.com>
2019-08-11 08:43:29 -0500

    Tag filtering

--- index.php
+++ index.php
@@ -2,11 +2,25 @@
 
 require_once("database.php"); 
 
-try {
-	$results = $db->query('select * from entries order by date desc');
-} catch(Exception $e) {
-	echo $e->getMessage();
-	die();
+if (isset($_GET["tag"])) {
+	try {
+		$results = $db->prepare('select entries.id, title, date from entries
+							    join entries_tags on entries.id = entries_tags.entry_id
+							    join tags on entries_tags.tag_id = tags.id
+							    where tag = ?');
+		$results->bindParam(1, $_GET["tag"]);
+		$results->execute();
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		die();
+	}
+} else {
+	try {
+		$results = $db->query('select * from entries order by date desc');
+	} catch(Exception $e) {
+		echo $e->getMessage();
+		die();
+	}
 }
 
 $entries = $results->fetchAll(PDO::FETCH_ASSOC);
@@ -29,19 +43,22 @@ $entries = $results->fetchAll(PDO::FETCH_ASSOC);
         <section>
             <div class="container">
                 <div class="entry-list">
-					<?php
+					<?php 
+					if (isset($_GET["tag"])) {
+						echo '<h3 style="text-align: center">Entries tagged "' . $_GET["tag"] . '"</h3><br>';
+					}
 					foreach($entries as $entry) {
 						$fullDate = date('F j, Y', strtotime($entry['date']));
 						try {
-							$results = $db->prepare('select tag from entries
+							$results = $db->prepare('select tag, tags.id from entries
 												   join entries_tags on entries.id = entries_tags.entry_id
 												   join tags on entries_tags.tag_id = tags.id
 												   where entries.id = ?');
 							$results->bindParam(1, $entry['id']);
 							$results->execute();
-							} catch(Exception $e) {
-								echo $e->getMessage();
-								die();
+						} catch(Exception $e) {
+							echo $e->getMessage();
+							die();
 						}
 						$tags = $results->fetchAll(PDO::FETCH_ASSOC);
 						echo <<<EOT
@@ -52,7 +69,7 @@ EOT;
 						if (!empty($tags)) {
 							echo '<div class="tag-container"><span class="tag-label">tags:</span>';
 							foreach($tags as $tag) {
-								echo '<span class="tag"><a href="index.php?search=' . $tag["tag"] .
+								echo '<span class="tag"><a href="index.php?tag=' . $tag["tag"] .
 								'">' . $tag["tag"] . '</a></span>';
 							}
 							echo('</div>');
